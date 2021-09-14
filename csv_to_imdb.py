bf6f3719be2b68ac6c91cd9df789107a7bd0e03c
fisheepx <poemfar@gmail.com>
2021-09-14 11:06:41 +0900

    对应新版豆瓣与IMDB页面

--- csv_to_imdb.py
+++ csv_to_imdb.py
@@ -6,14 +6,15 @@ import csv
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException  
+from selenium.common.exceptions import NoSuchElementException
 
 
 def login():
     driver = webdriver.Chrome()
     driver.get('https://www.imdb.com/registration/signin')
     element = driver.find_element_by_id('signin-perks')
-    driver.execute_script("arguments[0].setAttribute('style', 'color: red;font-size: larger; font-weight: 700;')", element)
+    driver.execute_script("arguments[0].setAttribute('style', 'color: red;font-size: larger; font-weight: 700;')",
+                          element)
     driver.execute_script("arguments[0].innerText = '请登录自己的IMDB账号, 程序将等待至登录成功。'", element)
     current_url = driver.current_url
     while True:
@@ -24,7 +25,8 @@ def login():
     print('IMDB登录成功')
     return driver
 
-def mark(is_unmark = False, rating_ajust = -1):
+
+def mark(is_unmark=False, rating_ajust=-1):
     driver = login()
     success_marked = 0
     success_unmarked = 0
@@ -35,7 +37,7 @@ def mark(is_unmark = False, rating_ajust = -1):
     with open(file_name, 'r') as file:
         content = csv.reader(file, lineterminator='\n')
         for line in content:
-            # 如果只标记为看过并没有过分则略过
+            # 如果只标记为看过并没有打过分则略过
             if not line[1]:
                 continue
             movie_name = line[0]
@@ -51,9 +53,9 @@ def mark(is_unmark = False, rating_ajust = -1):
             time.sleep(3)
             try:
                 if is_unmark:
-                    driver.find_element_by_class_name('star-rating-button')
+                    driver.find_element_by_xpath('//div[@data-testid="hero-rating-bar__user-rating__score"]')
                 else:
-                    driver.find_element_by_class_name('star-rating-text')
+                    driver.find_element_by_xpath('//div[@data-testid="hero-rating-bar__user-rating"]')
             except NoSuchElementException:
                 if is_unmark:
                     never_marked.append(f'{movie_name}({imdb_id})')
@@ -63,14 +65,19 @@ def mark(is_unmark = False, rating_ajust = -1):
                     print(f'已经在IMDB上打过分：{movie_name}({imdb_id})')
             else:
                 if is_unmark:
-                    driver.find_element_by_class_name('star-rating-button').click()
-                    driver.find_element_by_css_selector(f"[title^='Delete']").click()
+                    driver.find_element_by_xpath('//div[@data-testid="hero-rating-bar__user-rating"]').click()
+                    driver.find_element_by_xpath("//div[@class='ipc-starbar']/following-sibling::button[2]").click()
                     print(f'电影删除打分成功：{movie_name}({imdb_id})')
                     success_unmarked += 1
                 else:
-                    driver.find_element_by_class_name('star-rating-text').click()
-                    driver.find_element_by_css_selector(f"[title^='Click to rate: {movie_rate}']").click()
-                    print(f'电影打分成功：{movie_name}({imdb_id})')
+                    driver.find_element_by_xpath('//div[@data-testid="hero-rating-bar__user-rating"]').click()
+                    from selenium.webdriver.common.action_chains import ActionChains
+                    # 新版IMDB页面如果不先将鼠标移动到相应星星处再点击则则点击无效
+                    star_ele = driver.find_element_by_xpath(f'//button[@aria-label="Rate {movie_rate}"]')
+                    mark_action = ActionChains(driver).move_to_element(star_ele).click()
+                    mark_action.perform()
+                    driver.find_element_by_xpath("//div[@class='ipc-starbar']/following-sibling::button").click()
+                    print(f'电影打分成功：{movie_name}({imdb_id}) → {movie_rate}★')
                     success_marked += 1
             time.sleep(1)
     driver.close()
@@ -90,18 +97,16 @@ def mark(is_unmark = False, rating_ajust = -1):
 if __name__ == '__main__':
     if not os.path.exists(os.path.dirname(os.path.abspath(__file__)) + '/movie.csv'):
         print('未能找到CSV文件，请先导出豆瓣评分，请参照：',
-        'https://github.com/fisheepx/douban-to-imdb')
+              'https://github.com/fisheepx/douban-to-imdb')
         sys.exit()
-    if len(sys.argv) > 1 and sys.argv == 'unmark':
+    if len(sys.argv) > 1 and sys.argv[1] == 'unmark':
         mark(True)
     elif len(sys.argv) > 1:
         if sys.argv[1] not in ['-2', '-1', '0', '1', '2']:
             print('分数调整范围不能超过±2分(默认 -1分)，请参照：',
-            'https://github.com/fisheepx/douban-to-imdb')
+                  'https://github.com/fisheepx/douban-to-imdb')
             sys.exit()
         else:
             mark(False, sys.argv[1])
     else:
         mark()
-
-
--- douban_to_csv.py
+++ douban_to_csv.py
@@ -5,9 +5,11 @@ import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+             'Chrome/47.0.2526.106 Safari/537.36 '
 START_DATE = '20050502'
 IS_OVER = False
+
 
 def get_rating(rating_class):
     """
@@ -18,17 +20,19 @@ def get_rating(rating_class):
     """
     return int(rating_class[6])
 
+
 def get_imdb_id(url):
     r = requests.get(url, headers={'User-Agent': USER_AGENT})
     soup = BeautifulSoup(r.text, 'lxml')
-    info_aera = soup.find(id = 'info')
+    info_area = soup.find(id='info')
     imdb_id = None
     try:
-        if info_aera:
-            imdb_id = info_aera.find_all('a')[-1].text
-            if imdb_id and not imdb_id.startswith('tt'):
-                # 最后为官方小站的电影再往前取一个元素
-                imdb_id = info_aera.find_all('a')[-2].text
+        if info_area:
+            # 由于豆瓣页面更改，IMDB的ID处不再有链接更改查询方法
+            for index in range(-1, -len(info_area.find_all('span')) + 1, -1):
+                imdb_id = info_area.find_all('span')[index].next_sibling.strip()
+                if imdb_id.startswith('tt'):
+                    break
         else:
             print('不登录无法访问此电影页面：', url)
     except:
@@ -76,6 +80,7 @@ def get_info(url):
 
     return info
 
+
 def get_max_index(user_id):
     url = f"https://movie.douban.com/people/{user_id}/collect"
     r = requests.get(url, headers={'User-Agent': USER_AGENT})
@@ -89,11 +94,12 @@ def get_max_index(user_id):
     print(f'总共 {max_index} 页')
     return int(max_index)
 
+
 def url_generator(user_id):
     max_index = get_max_index(user_id)
     for index in range(0, max_index * 15, 15):
         yield f"https://movie.douban.com/people/{user_id}/collect" \
-                f"?start={index}&sort=time&rating=all&filter=all&mode=grid"
+              f"?start={index}&sort=time&rating=all&filter=all&mode=grid"
 
 
 def export(user_id):
@@ -106,13 +112,13 @@ def export(user_id):
         print(f'开始处理第 {page_no} 页...')
         info.extend(get_info(url))
         page_no += 1
-
     print(f'处理完成, 总共处理了 {len(info)} 部电影')
     file_name = os.path.dirname(os.path.abspath(__file__)) + '/movie.csv'
     with open(file_name, 'w') as f:
         writer = csv.writer(f, lineterminator='\n')
         writer.writerows(info)
     print('保存电影评分至：', file_name)
+
 
 def check_user_exist(user_id):
     r = requests.get(f'https://movie.douban.com/people/{user_id}/', headers={'User-Agent': USER_AGENT})
@@ -126,13 +132,13 @@ def check_user_exist(user_id):
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print('请输入豆瓣ID，关于如何运行此程序请参照：',
-        'https://github.com/fisheepx/douban-to-imdb')
+              'https://github.com/fisheepx/douban-to-imdb')
         sys.exit()
     if not check_user_exist(sys.argv[1]):
         print('请输入正确的豆瓣ID，如何查找自己的豆瓣ID 请参照：',
-        'https://github.com/fisheepx/douban-to-imdb')
+              'https://github.com/fisheepx/douban-to-imdb')
         sys.exit()
     if len(sys.argv) == 3:
         START_DATE = sys.argv[2]
-    print(f'开始抓取{START_DATE+"之后的" if START_DATE!="20050502" else "所有"}观影数据...')
+    print(f'开始抓取{START_DATE + "之后的" if START_DATE != "20050502" else "所有"}观影数据...')
     export(sys.argv[1])
